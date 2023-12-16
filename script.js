0638614065676f6e1cab2ed7e35513caee330681
Valéria Cristina <valeriac.dasilva26@gmail.com>
2023-12-16 18:15:46 -0300

    update js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ const objeto = {
 }
 return objeto;
 }
+
+const botao = document.querySelector('.botao');
+
+function mostrar() {
+  const texto = document.querySelector('.texto');
+  texto.classList.toggle('ativo');
+}
+
+botao.addEventListener('click', mostrar);
