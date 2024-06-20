6ead48fc6830d60f6b046cf7d4494ed82d0e9764
Valeria <valeriac.dasilva26@gmail.com>
2024-06-20 09:28:23 -0300

    JS update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,34 @@ if (url.includes(href)) {
 }
 
 links.forEach(ativarLink)
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto (parametro) {
+   const elemento = document.getElementById(parametro);
+   if (elemento) {
+      elemento.checked = true;
+   }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta (event) {
+   const pergunta = event.currentTarget;
+   const controls = pergunta.getAttribute('aria-controls');
+   const resposta = document.getElementById(controls);
+
+   resposta.classList.toggle('ativa');
+   console.log(controls);
+}
+
+function eventosPerguntas(pergunta) {
+   pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
